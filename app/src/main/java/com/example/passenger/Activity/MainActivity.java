699dc80ef0f873b5136c1542bef6fad85c5e0e85
riccardo.genova.igpo <riccardo.genova.igpo@outlook.com>
2024-10-25 09:29:37 +0200

    Aggiunta maschera login Inizio sviluppo maschera per collegamento a web servr

--- app/src/main/java/com/example/passenger/Activity/MainActivity.java
+++ app/src/main/java/com/example/passenger/Activity/MainActivity.java
@@ -5,11 +5,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,54 +27,57 @@ import com.example.passenger.Fragments.HomeFragment;
 import com.example.passenger.Fragments.PurchesFragment;
 import com.example.passenger.R;
 import com.example.passenger.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    private DrawerLayout drawerLayout_home;
+    BottomNavigationView bottomNavigationView;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    private AppBarConfiguration appBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout_home = findViewById(R.id.drawerLayout_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navigationView = findViewById(R.id.navigationView_main);
 
-        changeColorStatusBar();
-        //region Fragments
-        replaceFragment(new HomeFragment());
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            if(R.id.home == item.getItemId())
-            {
-                replaceFragment(new HomeFragment());
-            }
-            else if(R.id.purches == item.getItemId())
-            {
-                replaceFragment(new PurchesFragment());
-            }
+        setSupportActionBar(toolbar);
 
-            return true;
-        });
-        //endregion
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView_main);
+        NavController navController = navHostFragment.getNavController();
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout_home, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout_home.addDrawerListener(toggle);
+        toggle.syncState();
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_purches)
+                .setOpenableLayout(drawerLayout_home)
+                .build();
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout_home);
+
+//        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//        startActivity(intent);
     }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout,fragment);
-        fragmentTransaction.commit();
-    }
-
-    private void changeColorStatusBar()
-    {
-        Window window = this.getWindow();
-        // clear FLAG_TRANSLUCENT_STATUS flag:
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-        // finally change the color
-        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimaryDark));
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = ((NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentContainerView_main)).getNavController();
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 
 }
--- app/src/main/java/com/example/passenger/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/passenger/Fragments/HomeFragment.java
@@ -44,13 +44,6 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Toolbar toolbarHome = view.findViewById(R.id.toolbar_home);
-
-        toolbarHome.setTitleTextColor(Color.parseColor("#FFCDD2"));
-
-        ((MainActivity)getActivity()).setSupportActionBar(toolbarHome);
-
-
         cardView_circolazioneRealTime = view.findViewById(R.id.cardView_circolazioneRealTime);
         cardView_circolazioneRealTime.setOnClickListener(new View.OnClickListener() {
             @Override
